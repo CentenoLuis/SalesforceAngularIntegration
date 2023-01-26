1967085215c718ce967d78ca35082ad5a80c5ab7
Centeno Luis <luis001155ar@gmail.com>
2023-01-26 19:16:14 -0300

    Layout v1.0.0 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestComponentComponent } from './components/test-component/test-component.component';
 import { SalesforceOAuthService } from './services/salesforce-oauth.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [AppComponent, TestComponentComponent],
+  declarations: [AppComponent, TestComponentComponent, NavbarComponent, ToolbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
